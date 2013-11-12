b508cdeef69fcc1fc1b6c5646373562ae7124c05
jontrainor <jon.trainor@gmail.com>
2013-11-12 11:03:18 -0600

    setup script creates directories for vim sessions and persistent undo if they do not already exist

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+#initialize vim plugin submodules
 git submodule init
+
+#copy configs to home directory
 cp -rf ./.vimrc ~/
 cp -rf ./.vim ~/
 cp -rf ./.tmux.conf ~/
+
+#create directories for vim sessions and undo
+cd ~/.vim
+if [ ! -d ~/.vim/sessions ]; then
+	mkdir sessions
+fi
+
+if [ ! -d ~/.vim/undodir ]; then
+	mkdir undodir
+fi
+
