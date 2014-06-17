639bbf4249d3359123c24a02b60613b4a2ecbf70
jontrainor <jon.trainor@gmail.com>
2014-06-17 12:23:29 -0500

    added bash scripts to the repo and setup script now copies them into ~/bin. also bound a script for killing tmux windows that will decrement following window indexes and added some settings for clipboard support

--- setup.sh
+++ setup.sh
@@ -9,6 +9,12 @@ cp -rf ./.vimrc ~/
 cp -rf ./.vim ~/
 cp -rf ./.tmux.conf ~/
 
+#copy scripts to ~/bin
+if [ ! -d ~/bin ]; then
+	mkdir ~/bin
+fi
+cp -rf ./bin/* ~/bin
+
 #create directories for vim sessions and undo
 cd ~/.vim
 if [ ! -d ~/.vim/sessions ]; then
@@ -18,4 +24,3 @@ fi
 if [ ! -d ~/.vim/undodir ]; then
 	mkdir undodir
 fi
-
